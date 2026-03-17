efc4852afb61c4a67787438470f818ee59e0f6db
graphento <idc>
2026-03-17 17:58:59 +0500

    second task finished

--- src/index.js
+++ src/index.js
@@ -1,58 +1,57 @@
-import Card from './Card.js';
-import Game from './Game.js';
-import TaskQueue from './TaskQueue.js';
-import SpeedRate from './SpeedRate.js';
+import Card from "./Card.js";
+import Game from "./Game.js";
+import TaskQueue from "./TaskQueue.js";
+import SpeedRate from "./SpeedRate.js";
 
 // Отвечает является ли карта уткой.
 function isDuck(card) {
-    return card && card.quacks && card.swims;
+	return card && card.quacks && card.swims ? true : false;
 }
 
 // Отвечает является ли карта собакой.
 function isDog(card) {
-    return card instanceof Dog;
+	return card instanceof Dog ? true : false;
 }
 
 // Дает описание существа по схожести с утками и собаками
 function getCreatureDescription(card) {
-    if (isDuck(card) && isDog(card)) {
-        return 'Утка-Собака';
-    }
-    if (isDuck(card)) {
-        return 'Утка';
-    }
-    if (isDog(card)) {
-        return 'Собака';
-    }
-    return 'Существо';
+	if (isDuck(card) && isDog(card)) {
+		return "Утка-Собака";
+	}
+	if (isDuck(card)) {
+		return "Утка";
+	}
+	if (isDog(card)) {
+		return "Собака";
+	}
+	return "Существо";
 }
 
+class Duck extends Card {
+	constructor() {
+		super("Мирная утка", 2);
+	}
 
+	quacks() {
+		console.log("quack");
+	}
 
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+	swims() {
+		console.log("float: both;");
+	}
 }
 
-
-// Основа для собаки.
-function Dog() {
+class Dog extends Card {
+	constructor() {
+		super("Пес-бандит", 3);
+	}
 }
-
-
-// Колода Шерифа, нижнего игрока.
-const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-];
 
 // Колода Бандита, верхнего игрока.
-const banditStartDeck = [
-    new Card('Бандит', 3),
-];
+const banditStartDeck = [new Dog()];
 
+// Колода Шерифа, нижнего игрока.
+const seriffStartDeck = [new Duck(), new Duck(), new Duck()];
 
 // Создание игры.
 const game = new Game(seriffStartDeck, banditStartDeck);
@@ -62,5 +61,5 @@ SpeedRate.set(1);
 
 // Запуск игры.
 game.play(false, (winner) => {
-    alert('Победил ' + winner.name);
+	alert("Победил " + winner.name);
 });
